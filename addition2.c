84390f38d17eefe3948b5d1f4ce16bc2e57d28c3
jigar461 <141473578+jigar461@users.noreply.github.com>
2023-09-12 19:34:42 +0530

    Add files via upload

---
+++ addition2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int num1,num2;
+    printf("Enter the first number:");
+    scanf("%d",&num1);
+    printf("Enter the second number:");
+    scanf("%d",&num2);
+    printf("Addition is:%d",num1+num2);
+}
